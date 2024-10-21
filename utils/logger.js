2a78a76c77c839d7582f3cb0d86abec76fd6a888
Mohith Shrivastava <msrivastav13@gmail.com>
2024-10-21 10:28:17 -0400

    stream based logs

--- utils/logger.js
+++ utils/logger.js
@@ -2,20 +2,10 @@ import winston from 'winston';
 
 const logger = winston.createLogger({
     level: 'info',
-    format: winston.format.json(),
-    defaultMeta: { service: 'user-service' },
+    format: winston.format.simple(),
     transports: [
-        new winston.transports.File({ filename: 'error.log', level: 'error' }),
-        new winston.transports.File({ filename: 'combined.log' }),
+        new winston.transports.Console(),
     ],
 });
-
-if (process.env.NODE_ENV !== 'production') {
-    logger.add(
-        new winston.transports.Console({
-            format: winston.format.simple(),
-        })
-    );
-}
 
 export default logger;
