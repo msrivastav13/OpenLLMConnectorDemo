bd544196663d5907b010a50a64a624cff89410f9
Mohith Shrivastava <msrivastav13@gmail.com>
2024-10-25 14:07:36 -0400

    try alternate model

--- config/index.js
+++ config/index.js
@@ -6,7 +6,7 @@ export default {
     port: process.env.PORT || 3000,
     huggingFaceApiKey: process.env.HUGGING_FACE_API_KEY,
     huggingFaceApiUrl:
-        'https://api-inference.huggingface.co/models/meta-llama/Llama-3.1-8B-Instruct/v1/chat/completions',
+        'https://api-inference.huggingface.co/models/meta-llama/Llama-3.1-8B-Instruct/v1/',
     corsOptions: {
         origin: process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(',') : [],
         methods: ['POST'],
